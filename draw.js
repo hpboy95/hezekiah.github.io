e203134b2b5c3f9f6a7c0ffd1e1d9be149506043
hpboy95 <hpboy99@gmail.com>
2019-10-28 16:58:42 -0700

    Added snake square
    Added a small square for the snake and also fixed drawing.

--- draw.js
+++ draw.js
@@ -1,2 +1,11 @@
 const canvas = document.querySelector(".canvas");
-console.log(canvas);
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+(function setup() {
+    snake = new Snake();
+    snake.draw();
+}())
